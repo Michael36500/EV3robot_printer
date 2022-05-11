af54156c9b6894554ec7291d57295a215f8f0426
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-05-11 19:40:34 +0200

    asd

--- img2num.py
+++ img2num.py
@@ -1,14 +1,25 @@
 import cv2
 import numpy as np
 
+#####################
+# DĚLÁM ARRAY ČÍSEL #
+#####################
+
+out = []
+arli = []
+
 path = "img.jpg"
 
 img = cv2.imread(path, 0)
 img = img.astype(np.uint8)
 
 th, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
-
 for line in img:
     # print(line, "   *")
-    for segment in line:
-        print(segment)
+    for sgm in line:
+        if sgm == 255:
+            sgm = 1
+        arli.append (sgm)
+    print(arli)
+    # out.append (arli)
+# print(out)
