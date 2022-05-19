29622ce9fc2f6208b13c1eda959facadea004231
Michael36500 <104157791+Michael36500@users.noreply.github.com>
2022-05-19 19:17:43 +0200

    asdf

--- img2num.py
+++ img2num.py
@@ -4,116 +4,66 @@ from datetime import datetime
 
 ############################################################################################################################################################
 #!! pozor na orientaci, teď je normálně (hlavou vzhůru)
-# dám začátek programu
-Time = datetime.now()
-print(Time)
-# micstart = Time.microsecond
-# secstart = Time.second
 
 #####################
 # DĚLÁM ARRAY ČÍSEL #
 #####################
-out = []
-multiout = []
-arli = []
-
-path = "img3.jpg"
-
-img = cv2.imread(path, 0)
-img = img.astype(np.uint8)
-
-# protože pro další blok potřebuji jednodimenziální pole, ale může se hodit více, tak dělám pole OUT (jedno) a MULTIOUT (multi)
-
-
-th, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
-# print(img)
-for line in img:
-    # print(line, "   *")
-    for sgm in line:
-        if sgm == 255:
-            sgm = 1
-        arli.append (sgm)
-        out.append(sgm)
-    out.append(2)
-    arli.append(2)
-    # print(arli)
-    multiout.append (arli)
+def Nums(path):
+    out = []
+    multiout = []
     arli = []
-# out = multidimenzionální pole [[][]]
 
-# print(out)
+    img = cv2.imread(path, 0)
+    img = img.astype(np.uint8)
 
-############################
-# ZJEDNODUŠENÍ + ZRYCHLENÍ #
-############################    
+    # protože pro další blok potřebuji jednodimenziální pole, ale může se hodit více, tak dělám pole OUT (jedno) a MULTIOUT (multi)
+    th, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
+    # print(img)
+    for line in img:
+        for sgm in line:
+            if sgm == 255:
+                sgm = 1
+            arli.append (sgm)
+            out.append(sgm)
+        out.append(2)
+        arli.append(2)
+        multiout.append (arli)
+        arli = []
 
-inp = out
+    ############################
+    # ZJEDNODUŠENÍ + ZRYCHLENÍ #
+    ############################    
 
-out = []
+    inp = out
 
-hled = 0    # hledané
-cis = 0   # počet stejných za sebou
+    out = []
 
-
-
-
-for pos in inp:
-    # if pos == hled:
-    #     cis = cis + 1
-    # else:
-    #     hled = 1
-    #     vys.append (cis)
-    #     cis = 1
-    if pos == 2:
-        out.append (cis)
-        cis = 0
-        out.append (-1)
-        hled = 0
-        continue
+    hled = 0    # hledané
+    cis = 0   # počet stejných za sebou
 
 
-    if pos != hled:
-        hled = 1 if hled == 0 else 0 
-        out.append (cis)
-        cis = 0
-
-    cis = cis + 1
 
 
-out.append (cis)
-# for line in inp:
-#     for char in line:
-#         # if char == 2:
-#         #     out.append(p_rad)
-#         #     p_rad = 0
-#         if char == 2:
-#             out.append (p_rad)
-#             p_rad = 0
-#             out.append (-1)
-#             hled = 0
-#             continue
-
-#         if char != hled:
-#             hled = 1 if hled == 0 else 0
-#             out.append (p_rad)
-#             p_rad = 0
-
-#         p_rad = p_rad + 1
+    for pos in inp:
+        if pos == 2:
+            out.append (cis)
+            cis = 0
+            out.append (-1)
+            hled = 0
+            continue
 
 
-#         # if char == hled:
-#         #     p_rad = p_rad + 1
+        if pos != hled:
+            hled = 1 if hled == 0 else 0 
+            out.append (cis)
+            cis = 0
 
-#         # if char != hled:
-#         #     out.append(p_rad)
-#         #     p_rad = 0
-#             # hled = 1 if hled == 0 else 0
-# print(out)
+        cis = cis + 1
 
-# konec
-Time = datetime.now()
-print(Time)
-# micend = Time.microsecond
-# secend = Time.second
-# print(micend - micstart)
-# print(secend - secstart)
+
+    out.append (cis)
+    
+    return out
+
+print(Nums("img.jpg"))
+
